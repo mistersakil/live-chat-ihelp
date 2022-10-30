6fce1d89c9c54d24d71ec4a1fe269bd8be55d814
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-10-30 18:23:09 +0600

    query updated on log.php

--- log.php
+++ log.php
@@ -1,29 +1,30 @@
 <?php
 session_start();
-include('./libs/sqli-db.php');
-if($_SESSION['chat_id']!=''){
+// include('./libs/sqli-db.php');
+require('./libs/database.php');
+if ($_SESSION['chat_id'] != '') {
 	$chat_id = $_SESSION['chat_id'];
-	$sql = "SELECT * FROM `chat_customer` WHERE `chat_id` = '".$chat_id."'";
-	$result = mysqli_query($conn, $sql);
-	while($row = mysqli_fetch_assoc($result)){
-	?>
-		<div class='msgln'>
-			<span class='chat-time'><?php echo $row['date']; ?></span>
-			<b class='user-name'><?php echo $row['user']; ?></b> <?php echo $row['message']; ?><br>
-		</div>
-	<?php
+	$sql = "SELECT * FROM `chat_customer` WHERE `chat_id` = '" . $chat_id . "'";
+	$result = $conn->query($sql);
+	while ($row = ($result->fetch_assoc())) {
+?>
+<div class='msgln'>
+    <span class='chat-time'><?php echo $row['date']; ?></span>
+    <b class='user-name'><?php echo $row['user']; ?></b> <?php echo $row['message']; ?><br>
+</div>
+<?php
 	}
-} else if ($_SESSION['close_chat_id']!=''){
+} else if ($_SESSION['close_chat_id'] != '') {
 	$chat_id = $_SESSION['close_chat_id'];
-	$sql = "SELECT * FROM `chat_customer` WHERE `chat_id` = '".$chat_id."'";
-	$result = mysqli_query($conn, $sql);
-	while($row = mysqli_fetch_assoc($result)){
+	$sql = "SELECT * FROM `chat_customer` WHERE `chat_id` = '" . $chat_id . "'";
+	$result = $conn->query($sql);
+	while ($row = ($result->fetch_assoc())) {
 	?>
-		<div class='msgln'>
-			<span class='chat-time'><?php echo $row['date']; ?></span>
-			<b class='user-name'><?php echo $row['user']; ?></b> <?php echo $row['message']; ?><br>
-		</div>
-	<?php
+<div class='msgln'>
+    <span class='chat-time'><?php echo $row['date']; ?></span>
+    <b class='user-name'><?php echo $row['user']; ?></b> <?php echo $row['message']; ?><br>
+</div>
+<?php
 	}
 }
 ?>
