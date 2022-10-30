a74e62b0bfee3fabb0f0ac8f5fc1c350b8fe599b
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-10-30 18:24:54 +0600

    query updated on notification-search-counter.php

--- notification-search-counter.php
+++ notification-search-counter.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
-if(isset($_SESSION['userLiveChat'])){
-	include('./libs/chat-db.php');
+if (isset($_SESSION['userLiveChat'])) {
+	// include('./libs/chat-db.php');
+	require('./libs/database.php');
 	//Here send by is the receiver
-	$sql = "SELECT * FROM `internal_conversation_notification` WHERE `received_by` = '".$_SESSION['userLiveChat']."'";
-	$qry = mysql_query($sql);
+	$sql = "SELECT * FROM `internal_conversation_notification` WHERE `received_by` = '" . $_SESSION['userLiveChat'] . "'";
+	$qry = $conn->query($sql);
 	$text_message = '';
-	while($row = mysql_fetch_assoc($qry)){		
-		$text_message .= $row['send_by']."|";					
+	while ($row = ($qry->fetch_assoc())) {
+		$text_message .= $row['send_by'] . "|";
 	}
-	$text_message = substr($text_message,0,-1);
-    echo $text_message;
+	$text_message = substr($text_message, 0, -1);
+	echo $text_message;
 }
