7ea2c3c5021284605cec67c3c0beb6864a37db2e
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-10-30 18:17:19 +0600

    query updated on  autoloader.php

--- autoloader.php
+++ autoloader.php
@@ -1,32 +1,30 @@
 <?php
-include('./libs/sqli-db.php');
+// include('./libs/sqli-db.php');
+require('./libs/database.php');
 //$chat_id = $_SESSION['chat_id'];
 //echo "Need Correction";
 
 //$ssql = "DELETE FROM `agent_status` WHERE `agent_status`.`agent_id` NOT IN (SELECT `asterisk`.`vicidial_live_agents`.`user` FROM `asterisk`.`vicidial_live_agents` WHERE `asterisk`.`vicidial_live_agents`.`campaign_id` = 'EC')";
 
-//$result2 = mysqli_query($conn, $ssql);
+//$result2 = $conn->query( $ssql);
 
-$sql2 = "SELECT * FROM `asterisk`.`vicidial_live_agents` WHERE `asterisk`.`vicidial_live_agents`.`user` NOT IN (SELECT `agent_status`.`agent_id` FROM `chat`.`agent_status`) AND `asterisk`.`vicidial_live_agents`.`campaign_id` = 'EC'";
-$result3 = mysqli_query($conn, $sql2);
+$sql2 = "SELECT * FROM `vicidial_live_agents` WHERE `vicidial_live_agents`.`user` NOT IN (SELECT `agent_status`.`agent_id` FROM `chat`.`agent_status`) AND `vicidial_live_agents`.`campaign_id` = 'EC'";
+$result3 = $conn->query($sql2);
 $now_time = date('Y-m-d H:i:s');
-while($data = mysqli_fetch_assoc($result3)){
+while ($data = ($result3->fetch_assoc())) {
 	$now_id = $data['user'];
 	//$ssql2 = "INSERT INTO `agent_status` ('',`date`,`agent_id`,`agent_name`,`status`,`last_status_change`) VALUES ('','$now_time','$now_id','','0','$now_time')";
 	$ssql2 = "INSERT INTO `agent_status` VALUES ('','$now_time','$now_id','','0','$now_time')";
-	mysqli_query($conn, $ssql2);
+	$conn->query($ssql2);
 }
 
 $sql = "SELECT * FROM `agent_status` WHERE `status` = '1' ORDER BY `last_status_change` DESC";
-$result = mysqli_query($conn, $sql);
-$row_count = mysqli_num_rows($result);
-if($row_count>0){
-	while($row = mysqli_fetch_assoc($result)){
-		$sql2 = "UPDATE `chat_id` SET `agent_name` = '".$row['agent_id']."',`status` = 'Pending' WHERE `status` = 'NEW' LIMIT 1";
-		mysqli_query($conn, $sql2);
-		echo mysqli_affected_rows($conn)." Row Updated";
-		
+$result = $conn->query($sql);
+$row_count = ($result->num_rows);
+if ($row_count > 0) {
+	while ($row = ($result->fetch_assoc())) {
+		$sql2 = "UPDATE `chat_id` SET `agent_name` = '" . $row['agent_id'] . "',`status` = 'Pending' WHERE `status` = 'NEW' LIMIT 1";
+		$conn->query($sql2);
+		echo ($conn->affected_rows) . " Row Updated";
 	}
 }
-
-?>
