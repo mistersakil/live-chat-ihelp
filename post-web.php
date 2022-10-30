5e1e1d65d6b91c7accaaa3d62c7a7fe542616d48
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-10-30 18:28:17 +0600

    query updated on post-web.php

--- post-web.php
+++ post-web.php
@@ -1,21 +1,20 @@
 <?
 session_start();
-if(isset($_SESSION['name'])){
-	include('./libs/sqli-db.php');
+if (isset($_SESSION['name'])) {
+	// include('./libs/sqli-db.php');
+	require('./libs/database.php');
 	$text = $_POST['text'];
 	$chat_id = $_POST['chat_id'];
 	$user_fullname = $_POST['user_fullname'];
 	$now_time = date('Y-m-d H:i:s');
 	$check_sql = "SELECT * FROM `chat_id` WHERE `id` = '$chat_id' LIMIT 1";
-	$check_result = mysqli_query($conn, $check_sql);
+	$check_result = $conn->query($check_sql);
 	$result = mysqli_fetch_assoc($check_result);
-	if($result['status']!='CLOSE'){
+	if ($result['status'] != 'CLOSE') {
 		$sql = "INSERT INTO `chat_customer` (`date`,`chat_id`,`user`,`message`) VALUES ('$now_time','$chat_id','$user_fullname','$text')";
-		mysqli_query($conn, $sql);
-		
+		$conn->query($sql);
+
 		$sql2 = "INSERT INTO `chat_customer_notification` (`date`,`chat_id`,`user`,`message`) VALUES ('$now_time','$chat_id','$user_fullname','$text')";
-		mysqli_query($conn, $sql2);
+		$conn->query($sql2);
 	}
-	
 }
-?>
