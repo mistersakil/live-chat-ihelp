ef2d9990959b1a01693751e122578cb2671cd82c
Sakil Mahmud <sakil.diu.cse@gmail.com>
2022-11-03 17:24:30 +0600

    added dateTime on system & agent conversation

--- api/v1/get_icon.php
+++ api/v1/get_icon.php
@@ -1,21 +1,22 @@
 <style>
-    #iHelpChatConversationDetails::-webkit-scrollbar {
-        width: 1px;
-        background: rgba(100, 100, 100, .3);
-    }
+#iHelpChatConversationDetails::-webkit-scrollbar {
+    width: 1px;
+    background: rgba(100, 100, 100, .3);
+}
 
-    #iHelpChatConversationDetails::-webkit-scrollbar-track {
-        /* -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 255, 1); */
-        border-radius: 15px;
-    }
+#iHelpChatConversationDetails::-webkit-scrollbar-track {
+    /* -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 255, 1); */
+    border-radius: 15px;
+}
 
-    #iHelpChatConversationDetails::-webkit-scrollbar-thumb {
-        border-radius: 15px;
-        /* -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5); */
-        background: transparent;
-    }
+#iHelpChatConversationDetails::-webkit-scrollbar-thumb {
+    border-radius: 15px;
+    /* -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5); */
+    background: transparent;
+}
 </style>
-<img class="logo" src="https://ihelpbd.com/images_old/favicon.ico" alt="logo" style="width:50px; height: 50px; border-radius: 50%; position: absolute; right: 10px; bottom: 10px; cursor:pointer" />
+<img class="logo" src="https://ihelpbd.com/images_old/favicon.ico" alt="logo"
+    style="width:50px; height: 50px; border-radius: 50%; position: absolute; right: 10px; bottom: 10px; cursor:pointer" />
 
 <section id="iHelpLiveChatContainer" style="width: 300px;height:500px; display:flex; flex-direction: column;
     background: #bf1a1a; position: fixed; right: 70px; bottom: 0; color:white; font-size:16px" data-display="hidden">
@@ -152,7 +153,17 @@
                     font-size: 16px;
                     font-weight: 700;
                     font-style:italic;
-                    ">System</p>
+                    display:flex;
+                    justify-content:space-between;
+                    ">
+                        System
+                        <span class="dateTime" style="
+                        font-size: 10px;
+                        display:inline-block;
+                        text-align:right;
+                        "><?php echo date('M d, Y h:i A'); ?></span>
+
+                    </p>
                     <p>Please wait, we will come back you soon</p>
                 </div>
                 <!-- /#iHelpChatAgentCurrentMessage -->
